61ef5d3bc4b76e837a7085e90c67024b9b15d44b
adn87 <aaghadieh@gmail.com>
2023-10-13 12:34:39 -0400

    oop_coffee_machine_010
    - updated the coffee_machine project to use OOP.
    - code clean up

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu = Menu()
+coffee = CoffeeMaker()
+money = MoneyMachine()
+item = MenuItem
+
+thirsty = True
+while thirsty:
+    answer = input("what would you like? (espresso/latte/cappuccino): ").lower()
+    if answer == "off":
+        print(f"Coffee machine needs some rest. MACHINE {answer}")
+        thirsty = False
+    elif answer == "report":
+        coffee.report()
+        money.report()
+    else:
+        drink = menu.find_drink(answer)
+        if coffee.is_resource_sufficient(drink):
+            if money.make_payment(drink.cost):
+                coffee.make_coffee(drink)
